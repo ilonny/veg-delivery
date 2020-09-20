e85af3632e37ee4045c1002d1f3860dd8614ca3d
ilonny <lonnyfox@bk.ru>
2020-09-21 01:08:39 +0300

    splash screen ios

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './modules';
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <NavigationContainer>
