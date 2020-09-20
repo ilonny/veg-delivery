8d0cfcf8f587bee8fc61e7c3c40cac8a36e930b6
ilonny <lonnyfox@bk.ru>
2020-09-21 00:01:28 +0300

    start onboarding

--- mobile-app/src/modules/index.tsx
+++ mobile-app/src/modules/index.tsx
@@ -1,32 +1,72 @@
 import * as React from 'react';
 import {View, Text} from 'react-native';
-// import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
-const HomeScreen = () => {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home screen</Text>
-    </View>
-  );
-};
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+import {Onboarding} from './Onboarding';
 
-export type TPage = {
-  name: string;
-  component: any;
-};
+// const tabRoutes = () => {
+//   return (
+//     <Tab.Navigator
+//       initialRouteName="Home"
+//       tabBarOptions={{
+//         activeTintColor: '#e91e63',
+//       }}>
+//       <Tab.Screen
+//         name="Home"
+//         component={HomeScreen}
+//         options={{
+//           tabBarLabel: 'Home',
+//         }}
+//       />
+//       <Tab.Screen
+//         name="Home2"
+//         component={HomeScreen}
+//         options={{
+//           tabBarLabel: 'Home2',
+//         }}
+//       />
+//       <Tab.Screen
+//         name="Home3"
+//         component={HomeScreen}
+//         options={{
+//           tabBarLabel: 'Home3',
+//         }}
+//       />
+//     </Tab.Navigator>
+//   );
+// };
+// const HomeScreen = (props) => {
+//   return (
+//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+//       <Text>Home screen {props.num}</Text>
+//     </View>
+//   );
+// };
 
-export const pages: TPage[] = [
-  {
-    name: 'Меню',
-    component: HomeScreen,
-  },
-];
+// export type TPage = {
+//   name: string;
+//   component: any;
+// };
 
-export type RootStackParamList = {
-  Меню: {name: string};
-};
+// export const pages: TPage[] = [
+//   {
+//     name: 'Меню',
+//     component: HomeScreen,
+//   },
+// ];
 
-// const Stack = createStackNavigator<RootStackParamList>();
+// export type RootStackParamList = {
+//   Меню: {name: string};
+// };
+
 export const StackNavigator = () => {
-  return <HomeScreen></HomeScreen>;
+  return (
+    <>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+      </Stack.Navigator>
+    </>
+  );
 };
